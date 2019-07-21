3e69e0ccd8f07e4db357cfedc5d420dd8bc28116
Cha Yang <chachaiyang3@gmail.com>
2019-07-21 01:01:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function destructivelyPrependKitten(name){
 function destructivelyRemoveLastKitten(){
   kittens.pop();
 }
-function destructivelyFirstKitten(){
+function destructivelyRemoveFirstKitten(){
   kittens.shift();
 }
